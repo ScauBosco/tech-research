c9d11c259d2973083b0e16f9c973cab71d002fe6
黄嘉诚 <huangjiacheng01@corp.netease.com>
2022-09-06 10:29:37 +0800

    feat: 技术预研

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef, useEffect } from 'react';
+
+
+
 
 function App() {
+  const sb = useRef()
+  useEffect(() => {
+    // gsap.registerPlugin(ScrollTrigger);
+    gsap.from(sb.current, { duration: 5, scale: 1 });
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div style={{ height: "100vh" }}>123</div>
+      <div ref={sb}>
+        <img
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/logo-man.svg"
+          alt=""
+        />
+      </div>
+      <div style={{ height: "100vh" }}>456</div>
     </div>
   );
 }
